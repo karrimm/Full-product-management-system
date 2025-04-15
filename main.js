46fefe13d2201d85f0ea9cea3784ddef0dab35c3
karim <abdelkrim.bouadif@etu.uae.ac.ma>
2025-04-15 10:20:26 +0100

    js

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
+let title = document.getElementById('title')
+let price = document.getElementById('price')
+let taxes = document.getElementById('taxes')
+let ads = document.getElementById('ads')
+let discount = document.getElementById('discount')
+let total = document.getElementById('total')
+let count = document.getElementById('count')
+let category = document.getElementById('category')
+let submit = document.getElementById('submit')
+
+// console.log(title, price, taxes, ads, discount, total, count, category, submit)
+
 // get total price
+
+function getTotal(){
+    if (price.value != ''){
+        let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
+        total.innerHTML = result;
+    }
+}
+
+
+
+
+
 // create new product
 // save product in local storage
-// update product
-// delete product
-// search product
 // clear inputs
-// clear table
-
+// read
+// count
+// delete
+// update
+// search
+// clean data
